5b606634c052627935148402e2042ac22ce1ce48
Ludovic Ollagnier <ludovic@ollagnier.me>
2020-06-24 11:32:20 +0200

    Added tableView demo

--- Projects/App/App/ViewController.swift
+++ Projects/App/App/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        tableView.dataSource = self
+    }
+}
+extension ViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
+        //Section 0
+        //Ligne
+        //Ligne
+
+        //Section 1
+        //Ligne
+
+        switch section {
+        case 0:
+            return 5
+        case 1:
+            return 2
+        default:
+            return 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //IndexPath (section, row)
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "websiteCell", for: indexPath)
+
+        cell.textLabel?.text = "Apple Developer Portal"
+        cell.detailTextLabel?.text = "https://developer.apple.com"
+
+        return cell
+    }
 }
 
